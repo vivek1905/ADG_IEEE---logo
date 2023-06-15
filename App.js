7c0b07f4df296bb0fb5ce3f816fa77c3a0d902d6
Vivek Iyer <viyer1905@gmail.com>
2023-06-15 20:46:14 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form'
+import Navbar from './components/Navbar'
+import Landing from './components/Landingpage'
+
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Landing/>
+    <Form/>
+    
+    </>
+  );
+}
+
+export default App;
